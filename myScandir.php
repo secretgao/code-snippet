641e9a4d94bf222d2d26867283a148960c784a3c
secretgao <891841626@qq.com>
2017-02-07 18:02:52 +0800

    Update myScandir.php
    +++

--- myScandir.php
+++ myScandir.php
@@ -8,13 +8,13 @@
           //取出$dir中的所有文件
          $files = scandir($dir);
          //遍历输出
-         foreach($files as $file){
+         foreach ($files as $file) {
             //判断输出
-             if(is_dir($dir . '/' . $file)){    //../20150813
+             if (is_dir($dir . '/' . $file)) {    //../20150813
                  //路径:文件夹
                  echo '<font color="blue">',$file,'</font><br/>';
                  //文件夹下有子文件: . 和..除外
-                 if($file != '.' && $file != '..'){    
+                 if ($file != '.' && $file != '..') {    
                     //20150813等待-->回来:20150813文件夹全部被遍历
                      //20150814等待-->回来:20150814文件夹全部被遍历
                      //...
@@ -22,7 +22,7 @@
                      //递归点
                     myScandir($dir . '/' . $file);
                  }
-             }else{
+             } else {
                  //文件
                  echo '<font color="red">',$file,'</font><br/>';
              }
