d9a3f177a35231edae4e208efd186f116e92f7e5
高鑫宇 <891841626@qq.com>
2017-02-07 17:32:54 +0800

    +

---
+++ microtime.php
@@ -0,0 +1,15 @@
+<?php
+
+	$stime = microtime(true); #获取程序开始执行的时间
+
+    #你写的php代码
+
+    $etime = microtime(true); #获取程序执行结束的时间
+    $total = $etime - $stime;   #计算差值
+   
+    echo $total;
+
+
+
+
+?>
